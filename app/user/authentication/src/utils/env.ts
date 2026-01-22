78bdeffdb1e51518d6598c5953690674630e8c24
yait-nas <yait-nas@c3r9p8.1337.ma>
2026-01-22 11:05:28 +0100

    add: port in .env

--- app/user/authentication/src/utils/env.ts
+++ app/user/authentication/src/utils/env.ts
@@ -3,5 +3,6 @@ import	dotenv	from 'dotenv';
 
 dotenv.config();
 
-export	const	ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET ?? "ATSecret";
-export	const	REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET ?? "RTSECRET";
+export	const	ACCESS_TOKEN_SECRET	= process.env.ACCESS_TOKEN_SECRET ?? "ATSecret";
+export	const	REFRESH_TOKEN_SECRET	= process.env.REFRESH_TOKEN_SECRET ?? "RTSECRET";
+export	const	PORT			= Number(process.env.PORT ?? "3000");
