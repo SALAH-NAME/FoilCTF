acd4ef79cf2cb00700a05c0fb8133083bf9002d5
yait-nas <yait-nas@c3r8p10.1337.ma>
2026-01-24 15:17:48 +0100

    fix: tchach deleted and update .env.example

--- app/user/authentication/src/utils/types.ts
+++ app/user/authentication/src/utils/types.ts
@@ -3,10 +3,7 @@ import	express, {Request}	from 'express';
 
 export	interface User {
 	username:	string,
-<<<<<<< HEAD
 	email:		string,
-=======
->>>>>>> b810b2e9401a409782c9b37ff8e12aedd4a38dcd
 	password:	string
 }
 
--- app/user/authentication/src/utils/utils.ts
+++ app/user/authentication/src/utils/utils.ts
@@ -3,12 +3,9 @@ import	jwt, {VerifyErrors, VerifyCallback, JwtPayload}	from 'jsonwebtoken';
 import	express, { Request, Response, NextFunction }	from 'express';
 import	{ User, AuthRequest}				from './types';
 import	{ AccessTokenSecret, AccessTokenExpirationTime}	from './env';
-<<<<<<< HEAD
 import	validator					from 'email-validator';
 import	* as zod					from 'zod';
 import	{ users, posts}					from './db';
-=======
->>>>>>> b810b2e9401a409782c9b37ff8e12aedd4a38dcd
 
 export	function generateAccessToken(username: string) : string {
 	return jwt.sign({ username: username }, AccessTokenSecret, { expiresIn: AccessTokenExpirationTime as any});
@@ -35,7 +32,6 @@ export	function authenticateToken(req: AuthRequest, res: Response, next: NextFun
 		next();
 	}) satisfies VerifyCallback) // to check
 }
-<<<<<<< HEAD
 
 export	function	validateUserInput(req: AuthRequest): number {
 	if (req.body === undefined)
@@ -64,5 +60,3 @@ export	function	validateUserInput(req: AuthRequest): number {
 	}
 	return (0);
 }
-=======
->>>>>>> b810b2e9401a409782c9b37ff8e12aedd4a38dcd
