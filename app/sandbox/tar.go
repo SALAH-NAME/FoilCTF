d2ecc65a6cab24e87a0dbc3c70eb22250bdce1d9
Abderrahim Indjaren <rahimos.123@gmail.com>
2026-02-03 14:15:04 +0100

    fix: incorrect usage of defer in tar extraction

--- app/sandbox/tar.go
+++ app/sandbox/tar.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"archive/tar"
 	"io"
 	"mime/multipart"
 	"os"
 	"path/filepath"
 )
+
+func typeFlagString(typeFlag byte) string {
+	switch typeFlag {
+		case tar.TypeReg:
+			return "regular"
+		case tar.TypeRegA:
+			return "regular a"
+		case tar.TypeLink:
+			return "hard link"
+		case tar.TypeSymlink:
+			return "symbolic link"
+		case tar.TypeChar:
+			return "character device node"
+		case tar.TypeBlock:
+			return "block device node"
+		case tar.TypeDir:
+			return "directory"
+		case tar.TypeFifo:
+			return "FIFO node"
+		case tar.TypeCont:
+			return "<reserved>"
+		case tar.TypeXHeader:
+			return "PAX key-value"
+		case tar.TypeXGlobalHeader:
+			return "PAX key-value global"
+		case tar.TypeGNUSparse:
+			return "GNU sparse"
+		case tar.TypeGNULongName:
+			return "GNU meta file path"
+		case tar.TypeGNULongLink:
+			return "GNU meta file link"
+		default:
+			return fmt.Sprintf("<unknown %v>", typeFlag)
+	}
+}
+func tarExtractRegularFile(filePath string, r io.Reader) error {
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, r); err != nil {
+		return err
+	}
+	return nil
+}
 
 func TarExtract(archive multipart.File, destPath string) error {
 	r := tar.NewReader(archive)
@@ -20,19 +68,13 @@ func TarExtract(archive multipart.File, destPath string) error {
 		}
 
 		switch header.Typeflag {
-		case tar.TypeDir:
 		case tar.TypeReg:
 			regPath := filepath.Join(destPath, filepath.Clean(header.Name))
-
-			regFile, err := os.Create(regPath)
-			if err != nil {
+			if err := tarExtractRegularFile(regPath, r); err != nil {
 				return err
 			}
-			defer regFile.Close()
-
-			if _, err := io.Copy(regFile, r); err != nil {
-				return err
-			}
+		default:
+			return fmt.Errorf("cannot extract unsupported file type %q", typeFlagString(header.Typeflag))
 		}
 	}
 	return nil
