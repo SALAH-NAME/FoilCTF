1a16aaff68bbdf30f093524d952997199412adf6
Yasser Ait Nasser <143262386+yasseraitnasser@users.noreply.github.com>
2026-02-04 19:59:34 +0100

    User: Authentication (#101)
    * add: implement "Hello world!" demo
    * add: implement "Hello world!" demo
    * add: serve html files, implementing 42 oauth
    * add: authenticate with 42, need to implement logout to destroy cookie
    * add: implement cookie, split code
    * add: make code more readable
    * add: introduce proper cycle, organize code
    * add: more organize code
    * edit: remove extara imports taht didn't used.
    * add: start implement user authentication
    * add: back-up index.js file, install necessary packages
    * fix: file architec
    * add: isolate authorization from authentication, start recode authentication with TS
    * add: port in .env
    * add: authentication in TS, change "access token expiration time" to .env variable
    * add: authentication in TS, change "access token expiration time" to .env variable
    * add: user authentication requirements
    * fix: tchach deleted and update .env.example
    * add: drizzle config file and install necessary packages
    * fix: drizzle config typo, add: migration setup
    * fix: DATABASE_URL
    * add: drizzle orm
    * refactor: organize code, use User type from schema, let database do the heavy lifting
    * remove: service.ts
    * add: README file and remove unnecessary modules from package.json
    * refactor: match endpoints with google sheets
    * rm: oauth dir till fixed
    * fix: use proper SQL query, rm: drizzle dir
    * fix: use zod for parsing, let db do the heavy lifting
    * fix: refreshTokens to database
    * add: db sessions migration, accessTokens and completely move refreshToken from in-memory to db
    * refactor: use zod object for request schema
    * add: role column to users, token to cookies instead of body, rm accessTokens column from sessions, refactor: destructor user object to only use username in refreshToken and username and role in accessToken
    * refactor: refresh token expiration time as a env variable
    * refactor: use token in cookie instead of body and no longer use zod schema for refresh_logout, fix: use try catch in async functions
    * got rid of unused imports
    * add: id auto generated in db and rm more unused imports
    * fix: update .env.example
    * add: dummy hash, role init in db, username and email to lower case, rm unused code and commented one, refactor: throw excpetion when an env variable is missing
    * refactor: give default values to some env variables
    * fix: username toLower before storing in db
    * add: user ID to jwt tokens

---
+++ app/user/authentication/src/index.ts
@@ -0,0 +1,157 @@
+
+import	'dotenv/config';
+import	express, {Response, NextFunction}	from 'express';
+import	bcrypt						from 'bcrypt';
+import	jwt, {JwtPayload}	from 'jsonwebtoken';
+import	{ eq, or}						from 'drizzle-orm';
+import	{ users, sessions }				from './db/schema';
+import	{ db}						from './utils/db';
+import	ms, {StringValue}				from 'ms';
+import	{	RefreshTokenSecret,
+		PORT,
+		RefreshTokenExpiry
+	}						from './utils/env';
+import	{ ZodError}					from 'zod';
+import	cookieParser					from 'cookie-parser';
+import	{	User,
+		AuthRequest,
+		registerSchema,
+		loginSchema
+		}					from './utils/types';
+
+import	{
+		generateAccessToken,
+		validate}				from './utils/utils';
+
+
+const	app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.post('/api/auth/register',
+	validate(registerSchema),
+	async (req: AuthRequest, res: Response) => {
+	try {
+		const	{username, email, password} = req.body;
+		const   [existingUser] = await db.select()
+						.from(users)
+						.where(or(eq(users.username, username), eq(users.email, email)));
+		if (existingUser) {
+			res.sendStatus(409);
+			return ;
+		}
+		const	hashedPassword	= await bcrypt.hash(password, 10);
+		const	newUser: User	= {
+						username:	username,
+						email:		email,
+						password:	hashedPassword
+					  };
+		await db.insert(users).values(newUser);
+		console.log(`New user created: ${username}`);
+		res.sendStatus(201);
+	}
+	catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
+});
+
+app.post('/api/auth/login',
+	validate(loginSchema),
+	async (req: AuthRequest, res: Response) => {
+		const	{username, password} = req.body;
+		try {
+			const	[user] = await db.select().from(users).where(eq(users.username, username));
+			const	passwordIsValid = await bcrypt.compare(password,
+							user?.password ?? "$2b$10$dummyhashplaceholder");
+			if (user === undefined || !passwordIsValid) {
+				res.status(401).send('Invalid username or password');
+				return ;
+			}
+
+			const	accessToken		= generateAccessToken(user.username as any, user.role, user.id);
+			const	refreshToken		= jwt.sign(	{username: user.username, id: user.id},
+													RefreshTokenSecret,
+													{ expiresIn: RefreshTokenExpiry as any}
+								);
+			const	duration	= ms(RefreshTokenExpiry as StringValue);
+			const	expiryDate	= new Date(Date.now() + duration);
+			await db.insert(sessions).values({
+					refreshtoken:	refreshToken,
+					expiry:		expiryDate.toISOString(),
+					userId:		user.id,
+				});
+			res.cookie('jwt', refreshToken, {
+				httpOnly:	true,
+				secure:		true,
+				sameSite:	'strict',
+				maxAge:		duration
+			});
+			res.json({ accessToken: accessToken, refreshToken: refreshToken});
+		}
+		catch (err) {
+			console.error(err);
+			res.sendStatus(500);
+		}
+})
+
+app.post('/api/auth/refresh',
+	async (req: AuthRequest, res: Response) => {
+		try {
+			const	token = req.cookies?.jwt ?? "";
+			jwt.verify(token, RefreshTokenSecret) as JwtPayload;
+
+			const	[session] = await db.select()
+					.from(sessions).
+					where(eq(sessions.refreshtoken, token));
+			if (session === undefined) {
+				res.sendStatus(403);
+				return ;
+			}
+			const	[user] = await db.select()
+					.from(users)
+					.where(eq(users.id, session.userId));
+			if (user === undefined) {
+				res.status(400).send();
+				return ;
+			}
+			const	newAccessToken = generateAccessToken(user.username as string, user.role, user.id);
+			res.json({ accessToken: newAccessToken });
+		} catch (err) {
+			return res.sendStatus(403);
+		}
+	}
+)
+
+app.delete('/api/auth/logout',
+	async (req: AuthRequest, res: Response) => {
+		try {
+			const	token = req.cookies?.jwt;
+			if (token) {
+				await db.delete(sessions).where(eq(sessions.refreshtoken, token));
+				console.log('user session got deleted');
+			}
+
+			res.clearCookie('jwt', {
+				httpOnly:	true,
+				secure:		true,
+				sameSite:	'strict'
+			});
+			return res.sendStatus(204);
+		} catch (err) {
+			console.error(err);
+			return res.sendStatus(500);
+		}
+})
+
+app.use((err: any, req: AuthRequest, res: Response, next: NextFunction) => {
+	if (err instanceof ZodError) {
+		return res.sendStatus(400);
+	}
+	console.error(err);
+	res.sendStatus(500);
+})
+
+app.listen(PORT, () => {
+	console.log(`app listening on port ${PORT}`);
+});
---
+++ app/user/authentication/src/utils/utils.ts
@@ -0,0 +1,52 @@
+
+import	jwt, {VerifyErrors, VerifyCallback, JwtPayload}	from 'jsonwebtoken';
+import	{ Response, NextFunction }		from 'express';
+import	{ User, AuthRequest}				from './types';
+import	{ AccessTokenSecret, AccessTokenExpiry}		from './env';
+import	{ ZodObject }					from 'zod';
+
+export	function generateAccessToken(username: string, role: string, id: number) : string {
+	return jwt.sign(	{ username: username, role: role , id: id},
+				AccessTokenSecret,
+				{ expiresIn: AccessTokenExpiry as any}
+			);
+}
+
+export	function authenticateToken(req: AuthRequest, res: Response, next: NextFunction) : void {
+	const	authHeader = req.get('authorization');
+	if (authHeader === undefined) {
+		res.sendStatus(400);
+		return ;
+	}
+	const	[bearer, token]	= authHeader.split(' ');
+	if (bearer !== 'Bearer' || !token) {
+		res.sendStatus(401);
+		return ;
+	}
+
+	jwt.verify(token, AccessTokenSecret, ((err: VerifyErrors | null, payload?: JwtPayload | string | undefined) => {
+		if (err) {
+			res.sendStatus(403);
+			return ;
+		}
+		req.user = payload as User; // LGHALB ALLAH
+		next();
+	}) satisfies VerifyCallback)
+}
+
+export	const	validate = (schema: ZodObject) =>
+	async (req: AuthRequest, res: Response, next: NextFunction) =>
+	{
+		try {
+			await schema.parseAsync({
+				body:		req.body,
+				query:		req.query,
+				params:		req.params,
+				cookies:	req.cookies
+				});
+			return next();
+		}
+		catch (error) {
+			next(error);
+		}
+	}
