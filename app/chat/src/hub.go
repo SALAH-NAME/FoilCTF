efd4ebebc9b35e95bb12ec40006df7a02af76ae6
Assma Sabir <118391165+assmasabir@users.noreply.github.com>
2026-02-12 14:57:39 +0100

    Feat/chat (#109)
    * JWT, used gorilla mux, edited leaveEvent handler, userd a map to store connections
    * go fmt
    * edited broadcast function
    * go fmt
    * prevented message channel blocking
    * simpler readme.
    * added error return value to DbInit
    * fix:
    - Race Condition using SafeSend() SafeCclose().
    - Deadlock release mutex before sending messages.
    - Panic for Double-Close using sync.Once.
    - multiple connection (multiple-device) broadcasts.
    - go fmt .
    - unnecessary fmt.Sprintf.
    * update: rename SECRET_KEY to JWT_SECRET, add fail if not found/set.
    * fix: chat: typo Brust -> Burst
    * fix: chat: rename Readme.md -> README.md
    * add: chat: improve responses and logging
    * add: chat: healthcheck endpoint at /health

--- app/chat/src/hub.go
+++ app/chat/src/hub.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"strconv"
 	"sync"
-
-	"github.com/gorilla/websocket"
-	"gorm.io/gorm"
 )
 
 type Hub struct {
@@ -16,7 +16,7 @@ type Hub struct {
 	MessageChannel chan Message
 	Register       chan *Client
 	Unregister     chan *Client
-	Clients        map[*Client]bool
+	Clients        map[string]map[*Client]bool
 	Upgrader       websocket.Upgrader
 	Mutex          sync.Mutex
 }
@@ -28,53 +28,64 @@ func NewHub(database *gorm.DB, conf *Config) *Hub {
 		MessageChannel: make(chan Message, conf.GlobalBuffer),
 		Register:       make(chan *Client, conf.RegisterBuffer),
 		Unregister:     make(chan *Client, conf.RegisterBuffer),
-		Clients:        make(map[*Client]bool),
+		Clients:        make(map[string]map[*Client]bool),
 		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool { //temporary
+			CheckOrigin: func(r *http.Request) bool {
 				return true
 			},
 		},
 	}
 }
 
-func (h *Hub) ServeChat(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		log.Printf("HTTP ERROR: Method not allowed")
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+func GetUserInfo(r *http.Request) (string, string, string, error) {
+	userID, ok := r.Context().Value(userIDKey).(string)
+	if !ok {
+		return "", "", "", fmt.Errorf("UserId not found in this context")
 	}
-	userID := r.Header.Get("X-User-Id")
-	userRole := r.Header.Get("X-User-Role")
-	userName := r.Header.Get("X-User-Name")
+	userName, ok := r.Context().Value(usernameKey).(string)
+	if !ok {
+		return "", "", "", fmt.Errorf("Username not found in this context")
+	}
+	userRole, ok := r.Context().Value(userRoleKey).(string)
+	if !ok {
+		return "", "", "", fmt.Errorf("userRole not found in this context")
+	}
+	return userID, userName, userRole, nil
+}
 
-	conn, err := h.Upgrader.Upgrade(w, r, nil)
+func (h *Hub) ServeChat(w http.ResponseWriter, r *http.Request) {
+	userID, userName, userRole, err := GetUserInfo(r)
 	if err != nil {
-		log.Printf("ERROR: Upgrading http connection failed : %v", err)
-		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+		log.Printf("DEBUG: SERVER: user info required %v", err)
+		JSONError(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
 	roomIDStr := r.URL.Query().Get("room")
 	roomID, err := strconv.Atoi(roomIDStr)
 	if err != nil {
-		log.Printf("ERROR: Valid RoomID is required")
-		http.Error(w, "Valid RoomID is required", http.StatusBadRequest)
+		log.Printf("DEBUG: SERVER: Valid RoomID is required")
+		JSONError(w, "Valid RoomID is required", http.StatusBadRequest)
 		return
 	}
-
+	conn, err := h.Upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("DEBUG: SERVER: Upgrading http connection failed : %v", err)
+		JSONError(w, "Could not open websocket connection", http.StatusBadRequest)
+		return
+	}
 	joinedClient := NewClient(conn, h, userID, userRole, userName, roomID)
-	h.Register <- joinedClient
 	go joinedClient.WriteToConnectionTunnel()
 	go joinedClient.ReadFromConnectionTunnel()
+	h.Register <- joinedClient
 }
 
 func (h *Hub) TrackChannels() {
-
 	for {
 		select {
 		case eventMessage, ok := <-h.MessageChannel:
 			{
 				if !ok {
-					log.Print("Global Message channel closed")
+					log.Print("DEBUG: EVENTS: Global Message channel closed")
 					return
 				}
 				switch eventMessage.Event {
@@ -87,18 +98,18 @@ func (h *Hub) TrackChannels() {
 				case "delete":
 					HandleDeleteEvent(h, &eventMessage)
 				default:
-					log.Printf("ERROR: Unknown event type received %s", eventMessage.Event)
+					log.Printf("ERROR: EVENTS: Unknown event type received %q", eventMessage.Event)
 				}
 			}
 		case client := <-h.Register:
 			HandleJoinEvent(h, client)
 		case client := <-h.Unregister:
-			{
-				if _, ok := h.Clients[client]; ok {
-					HandleLeaveEvent(h, client)
-				}
-			}
+			HandleLeaveEvent(h, client)
 		}
-
 	}
+}
+
+func (h *Hub) ServeHealth(w http.ResponseWriter, r *http.Request) {
+	_ = r
+	w.WriteHeader(http.StatusOK)
 }
